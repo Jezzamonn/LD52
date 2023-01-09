8f8edee9042dde2a9e0976ae132549bdcb94b8bd
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-01-09 09:57:13 -0800

    Text for each level!

--- src/ts/game/levels.ts
+++ src/ts/game/levels.ts
@@ -17,7 +17,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Flower,
         ],
         song: 'farmin-chords',
-        text: 'There once was a teeny seed who wanted to grow into a beautiful flower.<br><br>Use the arrow keys to move and press down to plant yourself!'
+        text: 'There once was a Teeny Seed who wanted to grow into a beautiful flower.<br><br>Use the arrow keys to move and press down to plant yourself!',
     },
     {
         name: 'use-vine',
@@ -25,6 +25,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Vine,
             SeedType.Flower,
         ],
+        text: 'The Teeny Seed wandered the fields until he found a friend.',
     },
     {
         name: 'vine-misdirection',
@@ -33,6 +34,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Flower,
         ],
         song: 'farmin-default',
+        text: 'The two seeds worked together to find a good spot for the Teeny Seed to grow.',
     },
     {
         name: 'explode',
@@ -41,6 +43,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Bomb,
             SeedType.Flower,
         ],
+        text: 'As they were exploring, they came across a new seed, with a bright red glow. He looked scary!',
     },
     {
         name: 'explode-and-grow',
@@ -49,6 +52,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Bomb,
             SeedType.Flower,
         ],
+        text: `"Don't worry! I'm a friendly seed," said the Cherry Bomb Seed. "Let me help you find a place to grow!"`,
     },
     {
         name: 'dirt',
@@ -59,6 +63,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Dirt,
             SeedType.Flower,
         ],
+        text: `In the corner of the field, they found a dirty-looking seed. He looked sad. "Nobody's wanted to be friends with me before."<br><br>"You can be friends with us!" said the Teeny Seed.`,
     },
     {
         name: 'tunnel',
@@ -68,6 +73,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Dirt,
             SeedType.Flower,
         ],
+        text: `Now all the seeds were working together to get the Teeny Seed where he needed to be.`,
     },
     {
         name: 'bomb-maze',
@@ -78,6 +84,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Bomb,
             SeedType.Flower,
         ],
+        text: `They found themselves in a place where all the soil looked old. But the Teeny Seed could feel that there was a good spot to grow under the dirt.<br><br>"Let me handle this!" said the Cherry Bomb Seed.`,
     },
     {
         name: 'win',
@@ -98,6 +105,7 @@ export const LEVELS: LevelInfo[] = [
             SeedType.Flower,
         ],
         song: 'farmin-alt-chords',
+        text: `They finally made to the end of the game. Congratulations!`,
     },
 ]
 
